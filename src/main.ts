989671313caeb7d54cf44f8533b68f68cc979121
HappyAdebayo <ogbenihappy05@gmail.com>
2026-08-17 09:03:45 -0700

    fix: removing trailing slashes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://tasksync-vert.vercel.app/',
+    origin: 'https://tasksync-vert.vercel.app',
   });
   app.useGlobalPipes(
     new ValidationPipe({
